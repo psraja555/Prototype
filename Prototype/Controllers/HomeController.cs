c5ba73d4d0629f1454445dd2acd8d7d114937784
HAVE A GREAT DAY <HAVE A GREAT DAY@DESKTOP-S35HLTL>
2024-12-06 12:49:36 +0530

    Add project files.

---
+++ Prototype/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Prototype.Models;
+using System.Diagnostics;
+
+namespace Prototype.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Images()
+        {
+            return View();
+        }
+        public IActionResult Others()
+        {
+            return View();
+        }
+        public IActionResult Backtohome()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
